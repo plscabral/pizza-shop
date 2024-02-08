e8810bae698e8a14591e1bbb42d9d79eed0522aa
Paulo Cabral <paulo.luiz127@gmail.com>
2024-02-08 10:19:20 -0300

    .

--- src/pages/app/dashboard/revenue-chart.tsx
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -3,7 +3,6 @@ import {
   Line,
   LineChart,
   ResponsiveContainer,
-  Tooltip,
   XAxis,
   YAxis,
 } from 'recharts'
@@ -59,6 +58,9 @@ export function RevenueChart() {
                 })
               }
             />
+
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
             <Line
               type="linear"
               strokeWidth={2}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './layouts/app'
 import { AuthLayout } from './layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard'
 import { Orders } from './pages/app/orders'
 import { SignIn } from './pages/auth/sign-in'
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },

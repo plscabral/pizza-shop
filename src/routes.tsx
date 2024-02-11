f701b395db119d197ba11c23cdd2e11276943132
Paulo Cabral <paulo.luiz127@gmail.com>
2024-02-11 09:12:55 -0300

    .

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from './layouts/app'
-import { AuthLayout } from './layouts/auth'
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard'
 import { Orders } from './pages/app/orders'
